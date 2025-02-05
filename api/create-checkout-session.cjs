e55b231410ed8c916ab44cdcc7a9521158013f61
Chris1998c <contechristian.98@gmail.com>
2025-02-05 18:11:25 +0100

    Update 7.0

--- api/create-checkout-session.cjs
+++ api/create-checkout-session.cjs
@@ -2,7 +2,7 @@
 
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, { apiVersion: '2022-11-15' });
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
