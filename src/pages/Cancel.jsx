cf822dc18078651ee0c7dee1c93e73f6e0b076ac
Chris1998c <contechristian.98@gmail.com>
2025-02-06 19:24:04 +0100

    update12

--- src/pages/Cancel.jsx
+++ src/pages/Cancel.jsx
@@ -1,13 +1,12 @@
-import React from "react";
-import { Container, Button } from "react-bootstrap";
-import Link from "next/link";
+import Link from 'next/link';
+import { Container, Button } from 'react-bootstrap';
 
 const Cancel = () => {
   return (
     <Container className="my-5 text-center">
       <h1>Ordine Annullato</h1>
       <p>Hai annullato la transazione, se vuoi riprovare puoi tornare al carrello.</p>
-      <Link href="/cart" passHref>
+      <Link href="/cart">
         <Button variant="danger">Torna al Carrello</Button>
       </Link>
     </Container>
--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -1,13 +1,12 @@
-import React from "react";
-import { Container, Button } from "react-bootstrap";
-import Link from "next/link";
+import Link from 'next/link';
+import { Container, Button } from 'react-bootstrap';
 
 const Success = () => {
   return (
     <Container className="my-5 text-center">
       <h1>Grazie per il tuo acquisto!</h1>
       <p>Il tuo ordine è stato completato con successo.</p>
-      <Link href="/" passHref>
+      <Link href="/">
         <Button variant="primary">Torna alla Home</Button>
       </Link>
     </Container>
