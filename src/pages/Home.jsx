09e81a724a56d4a6d13938602e1d3cbb11b801d6
Chris1998c <contechristian.98@gmail.com>
2025-02-12 16:25:59 +0100

    CarMen12

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
         <Carousel.Item>
           <img
             className="d-block w-100 carousel-image"
-            src="/img1.png"
+            src="/Img1.png"
             alt="Slide 1"
           />
           <Carousel.Caption>
