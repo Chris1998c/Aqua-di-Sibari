865037cd359be393a71d1a9b5b7c7b19f9347dba
Chris1998c <contechristian.98@gmail.com>
2025-02-12 12:56:21 +0100

    Final 3.0

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
-// src/main.jsx (modifica)
+// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+
+// Importa Font Awesome dal pacchetto installato via npm
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 import { CartProvider } from './context/CartContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
